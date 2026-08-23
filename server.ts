18f81437d702c0e0d4eacdc10f0d47fb64a91b13
MrRat <42673924+MisterRat@users.noreply.github.com>
2026-08-23 12:39:54 -0500

    Add files via upload
    fix model size limit

--- server.ts
+++ server.ts
@@ -81,7 +81,8 @@ async function startServer() {
   const app = express();
   const PORT = 3000;
 
-  app.use(express.json({ limit: "50mb" }));
+  app.use(express.json({ limit: "500mb" }));
+  app.use(express.urlencoded({ limit: "500mb", extended: true }));
   app.use(cookieParser());
 
   // Health check endpoint
